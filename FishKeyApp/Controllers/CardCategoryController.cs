d6d04fee5970a3a845434960012318c54bc450c9
papaJAMES <grzeszut@gmail.com>
2023-03-30 20:36:25 +0200

    added a1 category and cardcontroler

---
+++ FishKeyApp/Controllers/CardCategoryController.cs
@@ -0,0 +1,22 @@
+﻿using FishKeyApp.Models;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace FishKeyApp.Controllers
+{
+    class CardCategoryController
+    {
+        public List<FlashCardModel> GetCategory(string categoryName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"FishKeyApp.Resources.CardCategories.{categoryName}.json";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<FlashCardModel>>(result);
+            }
+        }
+    }
+}
--- FishKeyApp/Models/FlashCardModel.cs
+++ FishKeyApp/Models/FlashCardModel.cs
@@ -1,12 +1,25 @@
-﻿namespace FishKeyApp.Models
+﻿using System.Text.Json.Serialization;
+
+namespace FishKeyApp.Models
 {
     public class FlashCardModel
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("category")]
         public string Category { get; set; }
+
+        [JsonPropertyName("polish")]
         public string Polish { get; set; }
+
+        [JsonPropertyName("english")]
         public string English { get; set; }
+
+        [JsonPropertyName("mp3Url")]
         public string Mp3Url { get; set; }
+
+        [JsonPropertyName("imgUrl")]
         public string ImgUrl { get; set; }
     }
 }
